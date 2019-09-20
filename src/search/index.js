03573f09e07d212ea3afb8cd371ef2b9b681d1c9
wangwentao <wwtwesdom@163.com>
2019-09-20 11:54:25 +0800

    动态 import

--- src/search/index.js
+++ src/search/index.js
@@ -5,11 +5,28 @@ import "../../commons"
 import girl from "./images/girl.jpg";
 import "./search.less"
 class Index extends React.Component{
+  constructor() {
+    super(...arguments);
+    this.state = {
+      Text: null
+    }
+  }
+  loadComponent() {
+    import("./text.js").then((Text) => {
+      this.setState({
+        Text: Text.default
+      })
+    })
+  }
   render() {
+    const { Text } = this.state;
     return <div className="search-text">
+      {
+        Text ? <Text/> : null
+      }
       Search Text
       <div className="child">child</div>
-      <img src={ girl } alt=""/>
+      <img src={ girl } onClick={this.loadComponent.bind(this)} alt=""/>
     </div>;
   }
 }
